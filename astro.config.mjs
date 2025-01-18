14f07b4ca8dcaaa1ea5ab5d46256129157caae30
Nahuel Silva <hola@nahuel-s.dev>
2025-01-18 00:50:57 -0300

    Adding basic configurations and assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern',
+          additionalData: `@use "/src/styles/main.scss" as *;`
+        },
+      },
+    },
+  },
+});
